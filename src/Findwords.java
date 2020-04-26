6fd6f08f287a7f3c4437ed82276e31707cf590ea
Kamil <kom767@mail.ru>
2020-04-26 20:54:43 +0300

    down

---
+++ src/Findwords.java
@@ -0,0 +1,19 @@
+import com.sun.corba.se.impl.resolver.SplitLocalResolverImpl;
+
+import java.util.*;
+
+public class Findwords {
+    public static void main(String[] args) {
+        String [] words = {"Kamil", "Sah", "Namena", "Ytes", "Kamil", "Ksrea", "Namena", "Wow", "Wow", "Tarna", "Keyro", "Sah"};
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words){
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+        System.out.println("____________________________________________________________________");
+
+        Set<String> unique = new HashSet<>(Arrays.asList(words));
+        System.out.println(unique);
+        System.out.println("____________________________________________________________________");
+    }
+}
---
+++ src/Phonebook.java
@@ -0,0 +1,40 @@
+import javax.jws.soap.SOAPBinding;
+import java.util.*;
+
+public class Phonebook {
+    public static void main(String[] args) {
+    Directory directory = new Directory();
+
+    directory.add("Ivanov", "8999123321");
+    directory.add("Bobik", "8912155326");
+    directory.add("Kokorin", "8917155552");
+    directory.add("Kokorin", "8913455672");
+    directory.add("Ivanov", "899999999");
+    directory.add("Voitenko", "899111111");
+    directory.add("Bobik", "89923231999");
+    directory.add("Kokorin", "8324325234");
+    System.out.println(directory.get("Kokorin"));
+    System.out.println(directory.get("Voitenko"));
+    System.out.println(directory.get("Ivanov"));
+    }
+}
+
+class Directory {
+    private Map<String, List<String>> directory_hm = new HashMap<>();
+    private List<String> phone_number_list;
+
+    public void add(String surname, String phone_number) {
+        if (directory_hm.containsKey(surname)) {
+            phone_number_list = directory_hm.get(surname);
+            phone_number_list.add(phone_number);
+            directory_hm.put(surname, phone_number_list);
+        } else {
+            phone_number_list = new ArrayList<>();
+            phone_number_list.add(phone_number);
+            directory_hm.put(surname, phone_number_list);
+        }
+    }
+    public List<String> get(String surname) {
+        return directory_hm.get(surname);
+    }
+}
